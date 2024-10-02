8da3fdf5acabe41542a4e1cad8f2245ba7c627b1
Guy Bianco IV <gjbiancoiv@gmail.com>
2024-10-02 11:28:25 -0400

    update Version model for JSON schema

--- models/Version.ts
+++ models/Version.ts
@@ -1,6 +1,7 @@
 export interface Version {
   readonly id: number;
+  parent_asset: number;
   product: string;
-  productVersion: string;
-  ghReleaseBranchUrl: string;
+  product_version: string;
+  gh_release_branch_url?: string;
 }

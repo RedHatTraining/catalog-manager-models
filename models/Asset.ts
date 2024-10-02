0ef42f3c4d64d13c497414b68b13be88bec33c86
Guy Bianco IV <gjbiancoiv@gmail.com>
2024-10-02 08:23:29 -0400

    update Asset model to match JSON output

--- models/Asset.ts
+++ models/Asset.ts
@@ -1,7 +1,12 @@
 export type Asset = {
-  readonly id: number;
+  readonly id?: number;
   sku: string;
-  product: string;
-  version: string;
-  edition: string;
+  team_name: string;
+  // product: string;
+  // product_version: string;
+  // title: string;
+  // edition_number: number;
+  // status_name: string;
+  version_count: number;
+  edition_count: number;
 };
